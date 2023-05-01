6d262a9bb6d4cacd4d5736168652cb37f65bcdec
Яницкий Александр Анатольевич <a.yanitskiy@els24.com>
2023-05-02 02:12:26 +0300

    bug fix

--- yii-app/frontend/widgets/views/_item.php
+++ yii-app/frontend/widgets/views/_item.php
@@ -5,6 +5,6 @@ use yii\helpers\Html;
 
 ?>
 
-<div class="col-6 g-0">
+<div class="col-12 g-0">
   <?= Html::a($model->title, "/catalog/{$model->slug}", ['class' => 'mb-1 gaz d-inline-block']) ?>
 </div>
