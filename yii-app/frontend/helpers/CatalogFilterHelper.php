7372289940b4665fcead45106c04fb9c7d358ce7
Яницкий Александр Анатольевич <a.yanitskiy@els24.com>
2025-03-14 04:49:51 +0300

    bugfix

--- yii-app/frontend/helpers/CatalogFilterHelper.php
+++ yii-app/frontend/helpers/CatalogFilterHelper.php
@@ -4,55 +4,104 @@ namespace frontend\helpers;
 
 use common\models\Gaz;
 use common\models\Manufacture;
+use common\models\MeasurementType;
 use common\models\search\ProductSearch;
 use yii\web\Controller;
+use Yii;
 
 class CatalogFilterHelper extends Controller
 {
+    /**
+     * @param ProductSearch $searchModel
+     * @return array|\yii\db\DataReader
+     * @throws \yii\db\Exception
+     */
     public static function findAvailableManufacturesIds(ProductSearch $searchModel)
     {
-        $params = \Yii::$app->request->queryParams;
+        $params = Yii::$app->request->queryParams;
 
         if ($searchModel->gaz_id) {
             unset($params['ProductSearch']['manufacture_id']);
 
             $ids = (new ProductSearch())->searchFront($params)->query->select(['product.id'])->column();
 
-            $manufactureAvailableIds = \Yii::$app->db->createCommand("
+            if ($ids) {
+                $manufactureAvailableIds = Yii::$app->db->createCommand("
                 SELECT m.id FROM `manufacture` m
                 LEFT JOIN product p ON p.manufacture_id = m.id
                 WHERE p.id IN (" . join(',', $ids) . ")
                 GROUP BY m.id
             ")->queryColumn();
-
+            } else {
+                $manufactureAvailableIds = null;
+            }
         } else {
-            $manufactureAvailableIds  = Manufacture::find()->select(['id'])->column();
+            $manufactureAvailableIds = Manufacture::find()->select(['id'])->column();
         }
 
         return $manufactureAvailableIds;
     }
 
+    /**
+     * @param ProductSearch $searchModel
+     * @return array|\yii\db\DataReader
+     * @throws \yii\db\Exception
+     */
     public static function findAvailableGazIds(ProductSearch $searchModel)
     {
-        $params = \Yii::$app->request->queryParams;
+        $params = Yii::$app->request->queryParams;
 
         if ($searchModel->manufacture_id) {
             unset($params['ProductSearch']['gaz_id']);
 
             $ids = (new ProductSearch())->searchFront($params)->query->select(['product.id'])->column();
 
-            $gazAvailableIds = \Yii::$app->db->createCommand("
+            if ($ids) {
+                $gazAvailableIds = Yii::$app->db->createCommand("
 				SELECT g.id FROM `gaz` g
                 LEFT JOIN product_gaz pg ON pg.gaz_id = g.id
                 LEFT JOIN product p ON p.id = pg.product_id
                 WHERE p.id IN (" . join(',', $ids) . ")
                 GROUP BY g.id
             ")->queryColumn();
-
+            } else {
+                $gazAvailableIds = null;
+            }
         } else {
-            $gazAvailableIds  = Gaz::find()->select(['id'])->column();
+            $gazAvailableIds = Gaz::find()->select(['id'])->column();
         }
 
         return $gazAvailableIds;
+    }
+
+    /**
+     * @param ProductSearch $searchModel
+     * @return array|\yii\db\DataReader
+     * @throws \yii\db\Exception
+     */
+    public static function findAvailableMeasurementTypeIds(ProductSearch $searchModel)
+    {
+        $params = Yii::$app->request->queryParams;
+
+        if ($searchModel->gaz_id || $searchModel->manufacture_id) {
+            unset($params['ProductSearch']['measurement_type_id']);
+
+            $ids = (new ProductSearch())->searchFront($params)->query->select(['product.id'])->column();
+
+            if ($ids) {
+                $measurementTypeIds = Yii::$app->db->createCommand("
+				SELECT m.id FROM `measurement_type` m
+                LEFT JOIN product p ON p.measurement_type_id = m.id
+                WHERE p.id IN (" . join(',', $ids) . ")
+                GROUP BY m.id
+            ")->queryColumn();
+            } else {
+                $measurementTypeIds = null;
+            }
+        } else {
+            $measurementTypeIds = MeasurementType::find()->select(['id'])->column();
+        }
+
+        return $measurementTypeIds;
     }
 }
--- yii-app/frontend/views/catalog/_filter.php
+++ yii-app/frontend/views/catalog/_filter.php
@@ -1,4 +1,5 @@
 <?php
+
 /* @var $this yii\web\View */
 /* @var $model common\models\search\ProductSearch */
 
@@ -11,139 +12,143 @@ use frontend\helpers\CatalogFilterHelper;
 use yii\bootstrap5\ActiveForm;
 use yii\bootstrap5\Html;
 
+$manufactureAvailableIds = CatalogFilterHelper::findAvailableManufacturesIds($model);
+$manufactureOption = ['' => ['label' => ' ']];
+
+if ($manufactureAvailableIds) {
+    foreach (Manufacture::getDropDownData(true) as $id => $label) {
+        if (!in_array($id, $manufactureAvailableIds) && !empty($id)) {
+            $manufactureOption[$id] = ['disabled' => true];
+        }
+    }
+}
+
+$gazAvailableIds = CatalogFilterHelper::findAvailableGazIds($model);
+$gazOption = ['' => ['label' => ' ']];
+
+if ($gazAvailableIds) {
+    foreach (Gaz::getDropDownData(true) as $id => $label) {
+        if (!in_array($id, $gazAvailableIds) && !empty($id)) {
+            $gazOption[$id] = ['disabled' => true];
+        }
+    }
+}
+
+$measurementTypeIds = CatalogFilterHelper::findAvailableMeasurementTypeIds($model);
+$measurementTypeOption = ['' => ['label' => ' ']];
+
+if ($measurementTypeIds) {
+    foreach (MeasurementType::getDropDownData(true) as $id => $label) {
+        if (!in_array($id, $measurementTypeIds) && !empty($id)) {
+            $measurementTypeOption[$id] = ['disabled' => true];
+        }
+    }
+}
+
 ?>
 
 <?php $form = ActiveForm::begin(['method' => 'get', 'action' => '/catalog']); ?>
 
+<?= $form->field($model, 'manufacture_id')->dropDownList(
+    Manufacture::getDropDownData(true),
+    ['class' => 'form-select', 'options' => $manufactureOption])
+?>
 
+<?= $form->field($model, 'gaz_id')->dropDownList(
+    Gaz::getDropDownData(true),
+    ['class' => 'form-select', 'options' => $gazOption])
+?>
 
-        <div class="mb-3 field-productsearch-manufacture_id">
-            <label class="form-label" for="productsearch-manufacture_id">Производитель</label>
-            <select id="productsearch-manufacture_id" class="form-select" name="ProductSearch[manufacture_id]">
-                <?php
-                $manufactureAvailableIds = CatalogFilterHelper::findAvailableManufacturesIds($model);
-                foreach (Manufacture::getDropDownData(true) as $id => $label) {?>
-                    <option
-                            value="<?= $id?>"
-                            label="<?= $label?>"
-                            <?= !in_array($id, $manufactureAvailableIds) && !empty($id) ? 'disabled' : ''?>
-                            <?= $id == $model->manufacture_id ? 'selected' : ''?>
-                    >
-                        <?= $label?></option>
-                <?php } ?>
-            </select>
-        </div>
-
-        <div class="mb-3 field-productsearch-gaz_id">
-            <label class="form-label" for="productsearch-gaz_id">Газ</label>
-            <select id="productsearch-gaz_id" class="form-select" name="ProductSearch[gaz_id]">
-                <?php
-                $gazAvailableIds = CatalogFilterHelper::findAvailableGazIds($model);
-                foreach (Gaz::getDropDownData(true) as $id => $label) {?>
-                    <option
-                        value="<?= $id?>"
-                        label="<?= $label?>"
-                        <?= !in_array($id, $gazAvailableIds) && !empty($id) ? 'disabled' : ''?>
-                        <?= $id == $model->gaz_id ? 'selected' : ''?>
-                    >
-                        <?= $label?></option>
-                <?php } ?>
-            </select>
-        </div>
-
-
-
-<?php if(0): ?>
+<?php if (0): ?>
     <?= $form->field($model, 'gaz_group_id')->dropDownList(
         GazGroup::getDropDownData(true),
         ['class' => 'form-select', 'options' => ['' => ['label' => ' ']]])
     ?>
 <?php endif; ?>
 
+<?= $form->field($model, 'measurement_type_id')->dropDownList(
+    MeasurementType::getDropDownData(true),
+    ['class' => 'form-select', 'options' => $measurementTypeOption])
+?>
 
-    <?= $form->field($model, 'measurement_type_id')->dropDownList(
-        MeasurementType::getDropDownData(true),
-        ['class' => 'form-select', 'options' => ['' => ['label' => ' ']]])
-    ?>
+<div class="mb-3 border p-1">
+    <label>Диапазон</label>
 
-    <div class="mb-3 border p-1">
-        <label>Диапазон</label>
-
-        <div class="row g-1">
-          <div class="col">
+    <div class="row g-1">
+        <div class="col">
             <?= $form->field($model, 'range_from')->input('number') ?>
-          </div>
-          <div class="col">
+        </div>
+        <div class="col">
             <?= $form->field($model, 'range_to')->input('number') ?>
-          </div>
-          <div class="col">
+        </div>
+        <div class="col">
             <?= $form->field($model, 'range_unit')->dropDownList(
                 ProductRange::getDropDownDataGroupCol('unit', true),
                 ['class' => 'form-select', 'options' => ['' => ['label' => ' ']]])
             ?>
-          </div>
         </div>
     </div>
+</div>
 
-<?php if(0): ?>
+<?php if (0): ?>
     <div class="mb-3 border p-1">
         <label>Разрешение</label>
 
         <div class="row g-1">
-          <div class="col">
-            <?= $form->field($model, 'resolution_from')->input('number') ?>
-          </div>
-          <div class="col">
-            <?= $form->field($model, 'resolution_to')->input('number') ?>
-          </div>
+            <div class="col">
+                <?= $form->field($model, 'resolution_from')->input('number') ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'resolution_to')->input('number') ?>
+            </div>
         </div>
     </div>
 <?php endif; ?>
 
 
-    <div class="mb-3 border p-1">
-        <label>Время отклика</label>
+<div class="mb-3 border p-1">
+    <label>Время отклика</label>
 
-        <div class="row g-1">
-          <div class="col">
+    <div class="row g-1">
+        <div class="col">
             <?= $form->field($model, 'response_time_from')->input('number') ?>
-          </div>
-          <div class="col">
+        </div>
+        <div class="col">
             <?= $form->field($model, 'response_time_to')->input('number') ?>
-          </div>
         </div>
     </div>
+</div>
 
-<?php if(0): ?>
+<?php if (0): ?>
     <div class="mb-3 border p-1">
         <label>Энергопотребление</label>
 
         <div class="row g-1">
-          <div class="col">
-            <?= $form->field($model, 'energy_consumption_from')->input('number') ?>
-          </div>
-          <div class="col">
-            <?= $form->field($model, 'energy_consumption_to')->input('number') ?>
-          </div>
+            <div class="col">
+                <?= $form->field($model, 'energy_consumption_from')->input('number') ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'energy_consumption_to')->input('number') ?>
+            </div>
         </div>
     </div>
 <?php endif; ?>
 
-    <div class="mb-3 border p-1">
-        <label>Диапазон температур</label>
+<div class="mb-3 border p-1">
+    <label>Диапазон температур</label>
 
-        <div class="row g-1">
-          <div class="col">
+    <div class="row g-1">
+        <div class="col">
             <?= $form->field($model, 'temperature_range_from')->input('number') ?>
-          </div>
-          <div class="col">
+        </div>
+        <div class="col">
             <?= $form->field($model, 'temperature_range_to')->input('number') ?>
-          </div>
         </div>
     </div>
+</div>
 
-    <div class="form-group">
-        <?= Html::submitButton('Поиск', ['class' => 'btn mt-3 w-100']) ?>
-    </div>
+<div class="form-group">
+    <?= Html::submitButton('Поиск', ['class' => 'btn mt-3 w-100']) ?>
+</div>
 
 <?php ActiveForm::end(); ?>
