ff49a8c004aca97a2000f578ff2505156eb38979
janickkiy <janickiy@mail.ru>
2025-11-11 11:32:28 +0300

    правка карточки бренда

--- yii-app/frontend/views/manufacture/slug.php
+++ yii-app/frontend/views/manufacture/slug.php
@@ -1,6 +1,8 @@
 <?php
+
 /* @var $this yii\web\View */
 /* @var $model common\models\Manufacture */
+
 use yii\helpers\Html;
 
 $this->title = $model->title;
@@ -15,35 +17,39 @@ if ($seo = $model->seo) {
 ?>
 
 <div class='<?= $this->context->id ?>-<?= $this->context->action->id ?> container'>
-  <h1><?= $seo->h1 ?? $this->title ?></h1>
+    <h1><?= $seo->h1 ?? $this->title ?></h1>
 
-  <div id="post" class="single-post show">
-    <div class="entry-summary seperate-border">
-      <div class="manufacture-header">
-        <div class="single-img">
-          <div class="img-main">
-            <?= Html::img($model->logoUrl, ['style' => "max-height: 100px;", 'loading' => "lazy", 'alt' => $model->title]) ?>
-          </div>
-        </div>
-        <div class="col">
-          <h3 id="manufacture-title"><?= $model->title ?></h3>
-          <?php if($model->country): ?>
-            <div id="manufacture-country">
-              <i class="icon ion-md-map"></i>
-              <?= $model->country ?>
+    <div id="post" class="single-post show">
+        <div class="entry-summary seperate-border">
+            <div class="manufacture-header">
+                <div class="single-img">
+                    <div class="img-main">
+                        <?= Html::img($model->logoUrl, ['style' => "max-height: 100px;", 'loading' => "lazy", 'alt' => $model->title]) ?>
+                    </div>
+                </div>
+                <div class="col">
+                    <h3 id="manufacture-title"><?= $model->title ?></h3>
+                    <?php if ($model->country): ?>
+                        <div id="manufacture-country">
+                            <i class="icon ion-md-map"></i>
+                            <?= $model->country ?>
+                        </div>
+                    <?php endif; ?>
+                    <div>
+                        <i class="icon ion-md-map"></i> <a href="<?= $model->url ?>"><?= $model->url ?></a>
+                    </div>
+                    <div>
+                        <i class="icon ion-md-map"></i> <a href="/catalog/manufacture/<?= $model->url ?>">Перейти в товары
+                            бренда <?= $model->title ?></a>
+                    </div>
+                </div>
             </div>
-          <?php endif; ?>
-          <div>
-            <i class="icon ion-md-map"></i> <a href="<?= $model->url ?>"><?= $model->url ?></a>
-          </div>
+            <div class="delitimer"></div>
+            <p id="manufacture-description">
+                <?= $model->description ?>
+            </p>
         </div>
-      </div>
-      <div class="delitimer"></div>
-      <p id="manufacture-description">
-      	<?= $model->description ?>
-      </p>
     </div>
-  </div>
 
 </div>
 
