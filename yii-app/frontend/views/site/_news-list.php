7ac510579bf9080b36d5bac5104e95be78d0b5a0
janickkiy <janickiy@mail.ru>
2025-11-25 11:18:33 +0300

    блок новости

--- yii-app/frontend/views/site/_news-list.php
+++ yii-app/frontend/views/site/_news-list.php
@@ -3,30 +3,12 @@
 /* @var $this yii\web\View */
 /* @var $model common\models\News */
 
-use common\helpers\Tools;
 use common\helpers\StringHelpers;
 
-?>
-<header class="entry-header">
-    <div class="entry-meta">
-        <header class="entry-header">
-            <div class="entry-meta">
-                    <span class="posted-on">
-                    <span class="entry-date published">
-                    <?php
-                    $time = strtotime($model->date);
-                    ?>
-                    <span class="date text-right">
-                    <?= date('d', $time) ?>
-                    <?= Tools::$months[date('n', $time) - 1] ?>
-                    <?= date('Y', $time) ?>
-                    </span>
-                    </span>
-            </div>
-        </header>
-    </div>
-</header>
+$time = strtotime($model->date);
 
+?>
 <p>
+    <b><?= date('d.m.Y', $time) ?> - </b>
     <a href="/news/<?= $model->slug ?>"><?= StringHelpers::shortText($model->seo->h1 ?? $this->title, 100) ?></a>
 </p>
